defa3db1ac892d4e1a98f880377636a0d794a0eb
Asif <asifali8562919531@gmail.com>
2026-06-16 23:09:21 +0530

    Add project files.

---
+++ Inventry.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Inventry.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
